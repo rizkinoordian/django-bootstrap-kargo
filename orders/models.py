8e3a89794947d73f627d55c2dfd114bad4801b9c
Unknown <haniffrizki@gmail.com>
2017-01-14 14:41:05 +0700

    pep8 formatting

--- orders/models.py
+++ orders/models.py
@@ -6,27 +6,32 @@ from django.db import models
 from decimal import Decimal
 
 # every Django model fields can be customized by re-use parameter under
-# simple dictionary format 
+# simple dictionary format
 AMOUNT_MONEY = {
-                'max_digits': 25, 'decimal_places': 2, 
-                'default': Decimal('0.00')
-               }
+    'max_digits': 25, 'decimal_places': 2,
+    'default': Decimal('0.00')
+}
+
 
 class CommonInfo(models.Model):
     """
     Abstract models contains mandatory fields on each models.
     Every class can have these fields by instantiate from this class.
     """
-    remarks = models.TextField(blank=True, verbose_name='Keterangan Tambahan')
+    remarks = models.TextField(
+        blank=True, verbose_name='Keterangan Tambahan')
 
-    created = models.DateTimeField(auto_now_add=True, verbose_name='Waktu Dibuat')
-    modified = models.DateTimeField(auto_now=True, verbose_name='Waktu diedit')
+    created = models.DateTimeField(
+        auto_now_add=True, verbose_name='Waktu Dibuat')
+    modified = models.DateTimeField(
+        auto_now=True, verbose_name='Waktu diedit')
     status = models.BooleanField(default=True)
 
     uuid = models.UUIDField(default=uuid.uuid4, editable=False)
 
     # flag for update, delete, sync and coming from cloud
-    is_delete = models.BooleanField(default=False, verbose_name='Soft Delete')
+    is_delete = models.BooleanField(
+        default=False, verbose_name='Soft Delete')
 
     class Meta:
         abstract = True
@@ -34,7 +39,7 @@ class CommonInfo(models.Model):
 
 class ODOrder(CommonInfo):
     """
-    Order details contain name, phone and amount of total price order 
+    Order details contain name, phone and amount of total price order
     """
     name = models.CharField(max_length=30)
     phone = models.CharField(max_length=30)
--- orders/urls.py
+++ orders/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     url(r'^management/$', order_views.management, name="management"),
 
     url(r'^create_order/$', order_views.create_order, name="create_order"),
-    url(r'^edit_order/(?P<uuid>[0-9a-z-]+)/$', order_views.edit_order, name="edit_order"),
-    url(r'^delete_order/(?P<uuid>[0-9a-z-]+)/$', order_views.delete_order, name="delete_order"),
+    url(r'^edit_order/(?P<uuid>[0-9a-z-]+)/$',
+        order_views.edit_order, name="edit_order"),
+    url(r'^delete_order/(?P<uuid>[0-9a-z-]+)/$',
+        order_views.delete_order, name="delete_order"),
 ]
